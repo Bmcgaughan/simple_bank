b8c0bbe865922b8e35d4e63f85838d41108a5e9e
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-09-09 20:14:11 -0400

    intial

---
+++ Makefile
@@ -0,0 +1,24 @@
+postgres:
+	docker run --name postgres -p 5431:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres
+
+createdb:
+	docker exec -it postgres createdb --username=root --owne=root simple_bank
+
+dropdb:
+	docker exec -it postgres dropdb simple_bank
+
+migrateup:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5431/simple_bank?sslmode=disable" -verbose up  
+
+migratedown:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5431/simple_bank?sslmode=disable" -verbose down
+
+sqlc:
+	sqlc generate
+
+test:
+	go test -v -cover ./...
+
+.PHONY: createdb dropdb postgres migrateup migreatedown sqlc test
+
+#
---
+++ db/util/random.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func randomString(n int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var sb strings.Builder
+	k := len(letters)
+	for i := 0; i < n; i++ {
+		c := letters[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return randomString(6)
+}
+
+func RandomMoney() int64 {
+	return randomInt(0, 10000)
+}
+
+func RandomCurrency() string {
+	const currencies = "USD, EUR, GBP, CHF, JPY, CAD, AUD, NZD"
+	currenciesList := strings.Split(currencies, ", ")
+	n := len(currenciesList)
+	return currenciesList[rand.Intn(n)]
+}
